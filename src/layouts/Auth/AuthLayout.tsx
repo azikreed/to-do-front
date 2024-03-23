f355d3634fcffbaba953872ebaeec1ee55a870b3
azikreed <azikreed@gmail.com>
2024-03-23 16:58:02 +0300

    [+] netlify error fixed

--- src/layouts/Auth/AuthLayout.tsx
+++ src/layouts/Auth/AuthLayout.tsx
@@ -1,8 +1,5 @@
-import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import styles from './AuthLayout.module.css';
-import Button from '../../components/Button/Button';
-import { useEffect } from 'react';
-import cn from 'classnames';
 
 export function AuthLayout() {
 	return <div className={styles['layout']}>
